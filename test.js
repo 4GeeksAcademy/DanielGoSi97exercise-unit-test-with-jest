42659d0459823927ec161780f926cdb80231856b
DanielGoSi97 <112876178+DanielGoSi97@users.noreply.github.com>
2026-01-15 00:24:08 +0000

    correcciones de logica

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
 const { fromYenToPound } = require('./app.js');
 
-test('100 Yen equal 87 Pounds', () => {
+test('100 Yen equal 0.56 Pounds', () => {
     let total = fromYenToPound(100);
-    expect(total).toBe(87);
+    expect(total).toBeCloseTo(0.56, 2);
 });
 
 const { fromEuroToDollar } = require('./app.js');
 
-test('100 Euro equal 87 Pounds', () => {
+test('100 Euros equal 107 Dollars', () => {
     let total = fromEuroToDollar(100);
-    expect(total).toBe(107);
+    expect(total).toBeCloseTo(107, 2);
 });
 
 const { fromDollarToYen } = require('./app.js');
 
-test('100 Dollar equal 87 Yen', () => {
+test('100 Dollars equal 14626.17 Yen', () => {
     let total = fromDollarToYen(100);
-    expect(total).toBe(15650);
+    expect(total).toBeCloseTo(14626.17, 2);
 });
